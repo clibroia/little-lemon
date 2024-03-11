4e1d0d8c67885a5c243acc7cbe33cf8b89a57efb
clibroia <carmine87.italy@gmail.com>
2024-03-11 12:35:36 +0100

    Added Header and Footer components to the BookingPage component

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 const BookingPage = () => {
     return(
         <>
-            Booking imported
+            <Header />
+            <Footer />
         </>
     );
 }
